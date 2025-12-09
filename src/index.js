bc4a8242691e7016f075b6350285f62836d49978
Arman Dezfuli <armandezfuli@proton.me>
2025-12-09 16:06:42 +0330

    chore: complete Redux fundamentals part 4 (store)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+// import App from './App'
+// import './api/server'
 
-import './api/server'
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import store from './store'
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './Reducer'
+import { print1, print2, print3 } from './exampleAddons/middleware'
+
+
+const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+
+// Pass enhancer as the second arg, since there's no preloadedState
+const store = createStore(rootReducer, middlewareEnhancer)
+
+export default store
